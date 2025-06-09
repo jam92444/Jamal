3a21bc9287bbd3b71c8a3e3e0aede80cac0190a9
jam92444 <jamaltwo.3121@gmail.com>
2025-06-09 15:16:20 +0530

    Footer link updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,23 +2,46 @@ import { assets } from "@/assets/assets";
 import Image from "next/image";
 import React from "react";
 
-const Footer = ({isDarkmode}) => {
+const Footer = ({ isDarkmode }) => {
   return (
     <div className="mt-20">
       <div className="text-center">
-        <Image src={ isDarkmode ? assets.logo_dark :  assets.logo} alt="" className="w-48 mx-auto mb-2" />
+        <Image
+          src={isDarkmode ? assets.logo_dark : assets.logo}
+          alt=""
+          className="w-48 mx-auto mb-2"
+        />
 
         <div className="w-max flex items-center gap-2 mx-auto">
-          <Image src={ isDarkmode? assets.mail_icon_dark : assets.mail_icon} alt="" className="w-6" />{" "}
+          <Image
+            src={isDarkmode ? assets.mail_icon_dark : assets.mail_icon}
+            alt=""
+            className="w-6"
+          />{" "}
           mdyusufwork@gmail.com
         </div>
       </div>
       <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
         <p>&copy; 2025 Md. Yusuf Jamal. All rights reserved. </p>
         <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
-            <li><a href="https://www.github.com/jam92444" target="_blank">Github</a></li>
-            <li><a href="https://www.linkedin.com/in/yusuf-jamal-106772293/" target="_blank">LinkedIn</a></li>
-            <li><a href="https://wa.me/918825512668" target="_blank">Whatsapp</a></li>
+          <li>
+            <a href="https://www.github.com/jam92444" target="_blank">
+              Github
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/mohamed-yusuf-jamal-106772293/"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a href="https://wa.me/918825512668" target="_blank">
+              Whatsapp
+            </a>
+          </li>
         </ul>
       </div>
     </div>
