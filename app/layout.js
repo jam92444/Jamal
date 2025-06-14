9455ff0147316f5d75abde6d241d2982145814e6
jam92444 <jamaltwo.3121@gmail.com>
2025-06-14 15:10:22 +0530

    SEO Completed

--- app/layout.js
+++ app/layout.js
@@ -154,24 +154,6 @@ export const metadata = {
   verification: {
     google: "w1OtGfNoOboAaLyWwZnhQdJYNwGp_rCVPyoTqh9fyWo",
   },
-  openGraph: {
-    title: "Mohamed Yusuf Jamal | Full Stack & React Developer Portfolio",
-
-    description:
-      "A full-fledged full stack developer showcasing frontend and backend projects.",
-    url: "https://mohamedyusufjamalportfolio.vercel.app/",
-    siteName: "Md. Yusuf Jamal Portfolio",
-    locale: "en_US",
-    type: "website",
-    images: [
-      {
-        url: "https://mohamedyusufjamalportfolio.vercel.app/",
-        width: 1200,
-        height: 630,
-        alt: "Md. Yusuf Jamal Portfolio",
-      },
-    ],
-  },
   twitter: {
     card: "summary_large_image",
     title: "Mohamed Yusuf Jamal | Full Stack & React Developer Portfolio",
@@ -201,6 +183,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Person",
+            name: "Mohamed Yusuf Jamal",
+            url: "https://mohamedyusufjamalportfolio.vercel.app",
+            sameAs: [
+              "https://github.com/jam92444",
+              "https://www.linkedin.com/in/mohamed-yusuf-jamal-106772293/",
+            ],
+            jobTitle: "Full Stack Developer",
+          }),
+        }}
+      />
+
       <body
         className={`${outfit.className} ${ovo.className} antialiased overflow-x-hidden leading-8 dark:bg-darkTheme dark:text-white`}
       >
